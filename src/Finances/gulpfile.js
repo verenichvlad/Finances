91c67641fc2acc4b46ac7b7c716d1f9117e48e47
verenichvlad <verenichvlad@gmail.com>
2016-03-09 00:42:03 +0100

    More gulp and angular configuration

--- src/Finances/gulpfile.js
+++ src/Finances/gulpfile.js
@@ -1,5 +1,60 @@
 ﻿var gulp = require('gulp');
 
+var devPath = './wwwroot/dev/';
+var appProd = './wwwroot/dest/';
+var stylesPath = './wwwroot/styles/';
+
+/* Mixed */
+var ext_replace = require('gulp-ext-replace');
+
+/* CSS */
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var cssnano = require('cssnano');
+
+/* JS & TS */
+var jsuglify = require('gulp-uglify');
+var typescript = require('gulp-typescript');
+
+/* Images */
+var imagemin = require('gulp-imagemin');
+
+var tsProject = typescript.createProject('tsconfig.json');
+
+gulp.task('build-css', function () {
+    return gulp.src(stylesPath + '*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([precss, autoprefixer, cssnano]))
+        .pipe(sourcemaps.write())
+        .pipe(ext_replace('.css'))
+        .pipe(gulp.dest(stylesPath));
+});
+
+gulp.task('build-ts', function () {
+    return gulp.src(devPath + '**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tsProject))
+        .pipe(sourcemaps.write())
+        //.pipe(jsuglify())
+        .pipe(gulp.dest(appProd));
+});
+
+gulp.task('build-img', function () {
+    return gulp.src(stylesPath + 'img/**/*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest(stylesPath + 'img/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(devPath + '**/*.ts', ['build-ts']);
+    gulp.watch(stylesPath + 'scss/**/*.scss', ['build-css']);
+    gulp.watch(stylesPath + 'img/*', ['build-img']);
+});
+
 gulp.task('moveToLibs', function (done) {
     gulp.src([
       'node_modules/angular2/bundles/js',
@@ -19,3 +74,5 @@ gulp.task('moveToLibs', function (done) {
       'node_modules/bootstrap/dist/css/bootstrap.css'
     ]).pipe(gulp.dest('./wwwroot/libs/css'));
 });
+
+gulp.task('default', ['watch', 'build-ts', 'build-css']);
---
+++ src/Finances/wwwroot/dev/app.component.ts
@@ -0,0 +1,12 @@
+﻿import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular 2 Boilerplate</h1>
+        <p>Hello World!</p>
+    `,
+})
+export class AppComponent {
+
+}
