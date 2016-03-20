0a7eabf9a3d107770ae87e885b587786fe852c31
verenichvlad <verenichvlad@gmail.com>
2016-03-21 00:10:48 +0100

    upd, new menu

--- src/Finances/wwwroot/app/app.component.js
+++ src/Finances/wwwroot/app/app.component.js
@@ -27,11 +27,8 @@ System.register(["angular2/core", "angular2/router", "./components/status.compon
                 }
                 AppComponent.prototype.ngOnInit = function () {
                     var menuEl = document.getElementById('ml-menu'), mlmenu = new MLMenu(menuEl, {
-                        // breadcrumbsCtrl : true, // show breadcrumbs
-                        // initialBreadcrumb : 'all', // initial breadcrumb text
                         backCtrl: false,
-                        // itemsDelayInterval : 60, // delay between each menu item sliding animation
-                        onItemClick: loadDummyData // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
+                        onItemClick: closeMenu()
                     });
                     // mobile menu toggle
                     var openMenuCtrl = document.querySelector('.action--open'), closeMenuCtrl = document.querySelector('.action--close');
@@ -42,18 +39,6 @@ System.register(["angular2/core", "angular2/router", "./components/status.compon
                     }
                     function closeMenu() {
                         classie.remove(menuEl, 'menu--open');
-                    }
-                    // simulate grid content loading
-                    var gridWrapper = document.querySelector('.content');
-                    function loadDummyData(ev, itemName) {
-                        ev.preventDefault();
-                        closeMenu();
-                        gridWrapper.innerHTML = '';
-                        classie.add(gridWrapper, 'content--loading');
-                        setTimeout(function () {
-                            classie.remove(gridWrapper, 'content--loading');
-                            gridWrapper.innerHTML = '<ul class="products">' + dummyData[itemName] + '<ul>';
-                        }, 700);
                     }
                     return null;
                 };
--- src/Finances/wwwroot/app/app.component.ts
+++ src/Finances/wwwroot/app/app.component.ts
@@ -23,14 +23,10 @@ declare var dummyData: any;
 
 export class AppComponent implements OnInit {
     ngOnInit(): any {
-
         var menuEl = document.getElementById('ml-menu'),
             mlmenu = new MLMenu(menuEl, {
-                // breadcrumbsCtrl : true, // show breadcrumbs
-                // initialBreadcrumb : 'all', // initial breadcrumb text
-                backCtrl: false, // show back button
-                // itemsDelayInterval : 60, // delay between each menu item sliding animation
-                onItemClick: loadDummyData // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
+                backCtrl: false,
+                onItemClick: closeMenu()
             });
 
         // mobile menu toggle
@@ -46,21 +42,6 @@ export class AppComponent implements OnInit {
 
         function closeMenu() {
             classie.remove(menuEl, 'menu--open');
-        }
-
-        // simulate grid content loading
-        var gridWrapper = <HTMLElement> document.querySelector('.content');
-
-        function loadDummyData(ev, itemName) {
-            ev.preventDefault();
-
-            closeMenu();
-            gridWrapper.innerHTML = '';
-            classie.add(gridWrapper, 'content--loading');
-            setTimeout(function () {
-                classie.remove(gridWrapper, 'content--loading');
-                gridWrapper.innerHTML = '<ul class="products">' + dummyData[itemName] + '<ul>';
-            }, 700);
         }
 
         return null;
